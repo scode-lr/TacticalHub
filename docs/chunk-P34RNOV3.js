0e7b9091ed74f12d6fe468e7d5826187d875b20d
sergilr <sergilr@SERGI>
2026-04-03 11:49:56 +0200

    migrate to docs

---
+++ docs/chunk-P34RNOV3.js
@@ -0,0 +1,1 @@
+import{b as a,c as b}from"./chunk-VGMJM2GZ.js";import"./chunk-4CLCTAJ7.js";export{a as GESTURE_CONTROLLER,b as createGesture};
---
+++ docs/chunk-WIG6WI5E.js
@@ -0,0 +1,1 @@
+var n=(function(e){return e.Active="AC",e.Pending="P",e.Draft="D",e.Inactive="I",e.Archived="AR",e})(n||{});export{n as a};
