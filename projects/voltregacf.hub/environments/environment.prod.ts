fc1030c018b900b48dad818b3605122c61107f3f
sergilr <sergilr@SERGI>
2026-04-03 12:23:17 +0200

    fix'

--- projects/voltregacf.hub/environments/environment.prod.ts
+++ projects/voltregacf.hub/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { PROJECT_CONFIG } from '../config';
 
 export const environment = {
   production: true,
-  apiUrl: 'https://foraminiferous-ethnocentric-collette.ngrok-free.dev',
+  apiUrl: 'http://tacticalhubapi-dev.eba-4eegbf9x.eu-south-2.elasticbeanstalk.com/',
   version: '1.0.0',
   ...PROJECT_CONFIG
 };
