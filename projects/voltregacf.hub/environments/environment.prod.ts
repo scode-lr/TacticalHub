7950caad995bf47e0141e9fe90d98aa13afa434e
sergilr <sergilr@SERGI>
2026-07-20 15:41:04 +0200

    fix: update apiUrl to correct endpoint in environment.prod.ts

--- projects/voltregacf.hub/environments/environment.prod.ts
+++ projects/voltregacf.hub/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { PROJECT_CONFIG } from '../config';
 
 export const environment = {
   production: true,
-  apiUrl: 'https://api-voltrega.tacticalhub.es',
+  apiUrl: 'https://api-voltregacf.tacticalhub.es',
   version: '1.0.0',
   ...PROJECT_CONFIG
 };
