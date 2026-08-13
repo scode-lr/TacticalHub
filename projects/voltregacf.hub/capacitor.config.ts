a4b52f810dd0c5fd90a810508a83588bd759e9d8
sergilr <sergilr@SERGI>
2026-08-13 18:26:51 +0200

    ffix notch

--- projects/voltregacf.hub/capacitor.config.ts
+++ projects/voltregacf.hub/capacitor.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="@capawesome/capacitor-android-edge-to-edge-support" />
+/// <reference types="@capacitor/status-bar" />
+
 import { CapacitorConfig } from '@capacitor/core';
 
 const config: CapacitorConfig = {
@@ -7,7 +10,18 @@ const config: CapacitorConfig = {
   server: {
     androidScheme: 'https'
   },
+  includePlugins: [
+    '@capawesome/capacitor-android-edge-to-edge-support',
+    '@capacitor/app',
+    '@capacitor/device',
+    '@capacitor/push-notifications',
+    '@capacitor/splash-screen',
+    '@capacitor/status-bar'
+  ],
   plugins: {
+    EdgeToEdge: {
+      backgroundColor: '#000000'
+    },
     PushNotifications: {
       presentationOptions: ['badge', 'sound', 'alert']
     }
