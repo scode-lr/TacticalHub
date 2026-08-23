7a79f4762eee6b16e2a369e8f565b2c436a55b80
Sergi <sergilr.code@gmail.com>
2026-08-23 12:22:03 +0200

    fix: respect mobile safe areas

--- projects/voltregacf.hub/environments/environment.dev.ts
+++ projects/voltregacf.hub/environments/environment.dev.ts
@@ -3,7 +3,7 @@ import { PROJECT_CONFIG } from '../config';
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7258',
+  apiUrl: 'http://192.168.1.51:5197',
   version,
   ...PROJECT_CONFIG
 };
